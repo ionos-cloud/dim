89cc188733d0dc735140e29611ba212676a3a63b
Stefan Radomski <stefan.radomski@ionos.com>
2020-11-17 10:26:11 +0100

    remove dependency on six
    As we are dropping the support for python2, we should also drop the
    dependencies on six as a tool to convert python2 codebases to python3.

--- dimclient/setup.py
+++ dimclient/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='dimclient',
       version='0.4.2',
-      install_requires=['simplejson', 'six'],
+      install_requires=['simplejson'],
       py_modules=['dimclient'])
