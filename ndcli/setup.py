6986f359c57b2afc080382a68d412c4eed612b94
Stefan Radomski <stefan.radomski@ionos.com>
2020-11-26 09:57:43 +0100

    add basic debian package building
    With the added functionality the debian buster packages are now built
    too for python3 only.

--- ndcli/setup.py
+++ ndcli/setup.py
@@ -1,25 +1,10 @@
 from setuptools import setup
 import codecs
 import os
-
-def read(rel_path):
-    here = os.path.abspath(os.path.dirname(__file__))
-    # intentionally *not* adding an encoding option to open, See:
-    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
-    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
-        return fp.read()
-
-
-def get_version(rel_path):
-    for line in read(rel_path).splitlines():
-        if line.startswith('__version__'):
-            # __version__ = "0.9"
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-    raise RuntimeError("Unable to find version string.")
+from dimcli import version
 
 setup(name='ndcli',
-      version=get_version('dimcli/__init__.py'),
+      version=version.VERSION,
       scripts=['ndcli'],
       install_requires=['dimclient>=0.4.1',
                         'python-dateutil',
