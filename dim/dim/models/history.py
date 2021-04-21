79628bae979506386235332259e04f543cb88c61
Stefan Radomski <stefan.radomski@ionos.com>
2021-04-21 12:36:48 +0200

    dim - revert history change for ipblock
    A separate handling was added to the history to fix an issue in
    history-ip.t. But this lead to the creation of a new column, which was
    not intended.
    So instead record history separately as is done in other places for
    ipblocks.

--- dim/dim/models/history.py
+++ dim/dim/models/history.py
@@ -69,8 +69,6 @@ def update_listener(mapper, connection, target):
         for change in list(changes.values()):
             if change.name == 'name':
                 make_history(target, 'renamed', {'name': change.old_value}, changed_attr=change)
-            elif change.name == 'ippool_id' and target.pool != None:
-                make_history(target, 'set_attr', changed_attr=AttrChange('pool', target.pool.display_name, change.old_value))
             else:
                 make_history(target, 'set_attr', changed_attr=change)
         del target._changed_attrs
@@ -253,7 +251,7 @@ generate_history_table(
      Column('vlan', Integer, info={'filler': default_filler('vlan')})] +
     generate_attr_change_columns(),
     [Ipblock.version, Ipblock.address, Ipblock.prefix, Ipblock.priority, Ipblock.gateway,
-     Ipblock.status, Ipblock.ippool_id, Ipblock.vlan],
+     Ipblock.status, Ipblock.pool, Ipblock.vlan],
     suppress_events=[Ipblock.version, Ipblock.address, Ipblock.prefix],
     indexes=[Index('ix_address_prefix_version', 'address', 'prefix', 'version')])
 
