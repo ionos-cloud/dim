2ecfd7cd0eeb4fc04bcf8cc330a4759af9c4272a
Nico Jehle <nicolas.jehle@ionos.com>
2021-12-22 15:53:39 +0100

    fix ldap_sync and ldap_auth for ldap3
    * `()` is not a valid filter in ldap3, require it for query()
    * specify attributes we want from LDAP for easier access
    * support setting TLS parameters for the LDAP connection from the config file
    * add minimal typing
    
    Allowing custom TLS parameters to support e.g. disabling verification in development
    environments, or using a specific certificate file to verify against.

--- dim/dim/ldap_auth.py
+++ dim/dim/ldap_auth.py
@@ -1,16 +1,27 @@
 import logging
 
 import ldap3
+import ssl
 from flask import current_app as app
 
+def check_credentials(username: str, password: str):
+    server_kwargs = {}
+    tls_kwargs = app.config.get_namespace('LDAP_SERVER_TLS_')
+    if app.config['LDAP_SERVER'].startswith('ldaps'):
+        if 'validate' in tls_kwargs.keys() and tls_kwargs.get('validate'):
+            tls_kwargs['validate'] = ssl.CERT_NONE
+        else:
+            tls_kwargs['validate'] = ssl.CERT_REQUIRED
+        server_kwargs['tls'] = ldap3.Tls(**tls_kwargs)
 
-def check_credentials(username, password):
+    ldap_server = ldap3.Server(app.config['LDAP_SERVER'], **server_kwargs)
+
     user_dn = app.config['LDAP_USER_DN'] % username
     if app.config['LDAP_SEARCH_BASE']:
         user_dn += "," + app.config['LDAP_SEARCH_BASE']
 
     server = ldap3.Server(app.config['LDAP_SERVER'])
-    conn = ldap3.Connection(server, user=user_dn, password=password, client_strategy=SAFE_SYNC)
+    conn = ldap3.Connection(ldap_server, user=user_dn, password=password, client_strategy=ldap3.SAFE_SYNC)
     if not conn.bind():
         logging.info('LDAP login for user %s failed: %s', username, conn.result)
         return False
