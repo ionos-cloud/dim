a6ad6d8214aa77d3adad5b978162f3c7c7458d0a
Nico Jehle <nicolas.jehle@ionos.com>
2022-03-01 13:42:36 +0100

    add legacy wrappers
    add wrappers for managing dim/db as drop-in for the old scripts

--- dim/setup.py
+++ dim/setup.py
@@ -5,4 +5,4 @@ setup(name='dim',
       version=version.VERSION,
       packages=['dim', 'dim.models'],
       package_data={'dim': ['sql/*.sql']},
-      scripts=['report'])
+      scripts=['report', 'manage_db', 'manage_dim'])
