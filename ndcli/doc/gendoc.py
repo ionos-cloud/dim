2aec9bde5fe9ed6520e0627ba18d3c606e32bb59
Nico Jehle <nicolas.jehle@ionos.com>
2022-06-07 10:21:39 +0200

    ndcli: fix gendoc for debian
    resolve issue locating the local dimcli module

--- ndcli/doc/gendoc.py
+++ ndcli/doc/gendoc.py
@@ -2,7 +2,7 @@ import sys
 import os
 # use path relative to doc/gendoc.py (this file)
 # to find its corresponding dimcli module
-ndcli_path = os.path.dirname(os.path.dirname(__file__))
+ndcli_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, ndcli_path)
 from dimcli import cmd
 
